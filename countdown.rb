90d2c55b7ef256f0f823485ab934c1d2f01ba8ca
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-31 04:13:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 
-def countdown
+def countdown(secs)
   secs == 10
     while secs > 0
       puts "#{secs} SECOND(S)!"
@@ -13,3 +13,4 @@ def countdown
 end
 
 def countdown_with_sleep(secs)
+end
