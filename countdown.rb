09229fe826891df40b87112b3e5013cbfd66c693
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-31 04:12:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,11 @@
 
 def countdown(secs)
   secs == 10
-  while secs > 0
-    puts "#{secs} SECOND(S)!"
-  secs -= 1
-  end
+    while secs > 0
+      puts "#{secs} SECOND(S)!"
+      secs -= 1
+    end
+    if secs == 10
     puts "HAPPY NEW YEAR!"
+  end
 end
