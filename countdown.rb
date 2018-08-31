b1cc87b74c49cfdbcf44df92b78d6f21414f36c2
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-31 03:49:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
-Time == 30
+
 
 def countdown
-  Time -= 1
+  Time == 30
   while Time > 0
     puts "#{Time} SECOND(S)! "
     until Time == 0
       puts "HAPPY NEW YEAR!"
+      Time -= 1
   end
 end
